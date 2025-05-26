9988b0ff69a6421df5e964200bd1fd491d5b7023
Temmarie <temmar_whyte16@yahoo.com>
2025-05-26 14:27:29 +0100

    Set up packages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/**/*.html",
+    "./*.html",
+    "./assets/js/**/*.js",
+    "./assets/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
