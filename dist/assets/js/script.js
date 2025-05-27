e359cab2ad9816f80b75502a1df47455c62e0419
Temmarie <temmar_whyte16@yahoo.com>
2025-05-27 15:24:07 +0100

    Made changes to logo

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -5,10 +5,10 @@ function carousel() {
       {
         image:
           "https://res.cloudinary.com/temmarie/image/upload/v1748248419/young-family-buying-car-dealer_f84fz0.webp",
-        title: "Expert UK Business Support",
+        title: "Expert UK Business Facilitation",
         subtitle: "Navigating the complexities of UK business operations",
         description:
-          "At WeRep, we provide expert guidance and support to help businesses thrive in the UK market. From logistics to investment facilitation, we've got you covered.",
+          "At WeRep, we provide expert guidance and facilitation to help businesses thrive in the UK market. From logistics to investment facilitation, we've got you covered.",
       },
       {
         image:
@@ -21,7 +21,7 @@ function carousel() {
       {
         image:
           "https://res.cloudinary.com/temmarie/image/upload/v1748248419/african-man-black-suit_tu3raq.webp",
-        title: "UK Business Representation",
+        title: "UK Corporate Representation",
         subtitle: "Expert representation for your business in the UK",
         description:
           "We provide expert representation services for businesses operating in the UK. Our team of experts will handle all aspects of your business operations, allowing you to focus on what matters most.",
